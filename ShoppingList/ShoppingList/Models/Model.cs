cb5451859f271d7056c489c06139d42959e3b413
T.Miyake <t@miyake.me>
2016-08-11 19:48:19 +0900

    Change sample application URL

--- ShoppingList/ShoppingList/Models/Model.cs
+++ ShoppingList/ShoppingList/Models/Model.cs
@@ -20,7 +20,7 @@ namespace ShoppingList
         private Model() { }
 
         // Insert your application URL.
-        // Sample "http://miyakeshoppinglist.azurewebsites.net"
+        // Sample "https://miyakeshoppinglist.azurewebsites.net"
         const string ApplicationUrl = "ApplicationUrl";
         readonly MobileServiceClient client = new MobileServiceClient(ApplicationUrl);
 
